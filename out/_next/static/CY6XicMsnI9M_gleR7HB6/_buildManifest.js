102a75210e10c7104ed4142655603207538bfc84
AustinW <51890817+rusty1996@users.noreply.github.com>
2021-09-01 01:29:03 -0400

    Hotfix updated nextjs and npm packages. Attempting to deploy to Vercel again.

---
+++ out/_next/static/CY6XicMsnI9M_gleR7HB6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/73-cd7ba257be3a9d431bb2.js","static/chunks/pages/index-a116fa4fc2a00f05080b.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
